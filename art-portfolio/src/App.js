0e21ccfc9b7a2e144caaa3353f6d8c8da3b5961f
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2023-10-06 17:13:00 -0400

    conditional rendering

--- art-portfolio/src/App.js
+++ art-portfolio/src/App.js
@@ -1,16 +1,45 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+
+// Header
+import Header from "./components/Header";
+// Navigation
+import Navigation from "./components/Nav";
+// About || Home
 import About from "./pages/About";
+//
 import Portfolio from "./pages/Portfolio";
 
+// Footer
+import Footer from "./components/Footer";
+
+// Use conditional rendering
+
 function App() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+}
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="body" style={{backgroundColor: "white"}}>
+      <Header />
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+      <Footer />
+    </div>
   );
 }
 
